f31f1508fe2e9bc322d095a4a97771240e9359d4
HarrisonChe5188 <144181530+HarrisonChe5188@users.noreply.github.com>
2025-12-11 20:40:27 -0500

    Clean up next.config.ts by removing rewrites
    Removed development rewrites from next.config.ts.

--- spark-bytes-team-10/next.config.ts
+++ spark-bytes-team-10/next.config.ts
@@ -1,17 +1,5 @@
 import type { NextConfig } from "next";
 
-const dev = process.env.NODE_ENV === "development";
-
-const nextConfig: NextConfig = {
-  async rewrites() {
-    if (!dev) return []; // No rewrites in production
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*',
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
