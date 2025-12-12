9576563c411c7956f5ec2180427bb522da211ba7
HarrisonChe5188 <144181530+HarrisonChe5188@users.noreply.github.com>
2025-12-11 20:25:36 -0500

    Add condition to disable rewrites in production
    Prevent rewrites in production environment.

--- spark-bytes-team-10/next.config.ts
+++ spark-bytes-team-10/next.config.ts
@@ -1,14 +1,17 @@
 import type { NextConfig } from "next";
 
+const dev = process.env.NODE_ENV === "development";
+
 const nextConfig: NextConfig = {
   async rewrites() {
+    if (!dev) return []; // No rewrites in production
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*'
-      }
+        destination: 'http://localhost:8000/api/:path*',
+      },
     ];
-  }
+  },
 };
 
 export default nextConfig;
