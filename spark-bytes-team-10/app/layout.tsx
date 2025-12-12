cdcdbbca34c14754cc51cf58f09b87202908dd31
HarrisonChe5188 <144181530+HarrisonChe5188@users.noreply.github.com>
2025-12-11 21:22:50 -0500

    Update layout.tsx

--- spark-bytes-team-10/app/layout.tsx
+++ spark-bytes-team-10/app/layout.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "next-themes";
 import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
+  ? `${process.env.NEXT_PUBLIC_SITE_URL}`
   : "http://localhost:3000";
 
 export const metadata: Metadata = {
